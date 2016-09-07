7d4201add5fe14a420a17c2d78c76a780a14f9ea
dowdje <dowdje89@gmail.com>
2016-09-07 17:08:42 -0400

    Fleshed out Followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :followships
-  has_many :followers, through: :followships, foreign_key: 'follower_id'
-  has_many :followings, through: :followships, foreign_key: 'following_id' 
+  has_many :followships, foreign_key: 'follower_id'
+  # has_many :followers, through: :followships, foreign_key: 'follower_id'
+  # has_many :followings, through: :followships, foreign_key: 'following_id'
 	has_many :votes
 	has_many :posts
   has_many :comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907175720) do
+ActiveRecord::Schema.define(version: 20160907194718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "followships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "following_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -24,12 +39,33 @@ ActiveRecord::Schema.define(version: 20160907175720) do
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.integer "tag_id",  null: false
+    t.integer "post_id", null: false
+    t.index ["post_id", "tag_id"], name: "index_posts_tags_on_post_id_and_tag_id", using: :btree
+    t.index ["tag_id", "post_id"], name: "index_posts_tags_on_tag_id_and_post_id", using: :btree
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.integer  "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "posts", "users"
