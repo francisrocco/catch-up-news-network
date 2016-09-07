0172398aeb345cf801c87ca823db4736952171e6
Francis Rocco <roccofrancis@gmail.com>
2016-09-07 14:24:56 -0400

    add create post functionality with show and index pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,25 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
 
   def new
     @post = Post.new
   end
 
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -14,6 +29,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :link)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,4 @@ class User < ApplicationRecord
 	has_many :posts
 	has_secure_password
 
-
-
-
 end
