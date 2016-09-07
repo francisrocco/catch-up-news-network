ff88c9ae2a6f60cd950c583559560561136003f3
Tami <catquarks@gmail.com>
2016-09-07 14:09:35 -0400

    add pry and faker to gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,22 @@
   #   t.string   "email"
   #   t.datetime "created_at", null: false
   #   t.datetime "updated_at", null: false
+require 'faker'
+
+
+# making users
+user = User.create(name: "George", email: "george@thebeatles.com")
+user = User.create(name: "John", email: "john@thebeatles.com")
+user = User.create(name: "Ringo", email: "ringo@thebeatles.com")
+user = User.create(name: "Paul", email: "paul@thebeatles.com")
+
+# I hard-coded the email addresses because I had a hunch they ought to be unique
+User.create(name: Faker::Pokemon.name.downcase + "gurl_" + Faker::Number.number(4), email: honeybear@booboo.com )
+User.create(name: Faker::Pokemon.name.downcase + "_gurl" + Faker::Number.number(3), email: diamond@rock.com )
+User.create(name: Faker::Pokemon.name.downcase + "gurl_" + Faker::Number.number(6), email: cowssaymoo@gmail.com )
+
+5.times do 
+
+User.new(name: Faker::)
+
+end
