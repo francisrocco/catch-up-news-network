709a471ae1b4f6d11c997346bccc25e2d1cfcf6b
Francis Rocco <roccofrancis@gmail.com>
2016-09-08 16:59:50 -0400

    add photo to thumbnail for websites without one

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.paginate(:page => params[:page]|| 1, :per_page => 5)
+    @posts = Post.paginate(:page => params[:page]|| 1, :per_page => 5).order('created_at DESC')
   end
 
   def show
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
     if @post.save
       redirect_to post_path(@post)
     else
