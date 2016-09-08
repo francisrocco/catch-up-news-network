6d1f5d23c6ec12ccd8208ab77dfd76bacdbf35da
Francis Rocco <roccofrancis@gmail.com>
2016-09-08 10:23:11 -0400

    create thumbnail of article

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,28 +1,25 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-# USING ONEBOX
-
-  def preview
-    Onebox.preview(self.link).to_s
-  end
-
-# USING THUMBNAILER
-
   def thumbnail
     object = LinkThumbnailer.generate(self.link)
   end
 
   def post_title
-    self.thumbnail.title
+    title = self.thumbnail.title
+    if title.length < 40
+      return title
+    else
+      return title[0..37] + "..."
+    end
   end
 
   def description
     desc = self.thumbnail.description
-    if desc.length < 140
+    if desc.length < 100
       return desc
     else
-      return desc[0..135] + "..."
+      return desc[0..97] + "..."
     end
   end
 
