9da66b262f92f9ccd15a6439df51c9c2699a4175
dowdje <dowdje89@gmail.com>
2016-09-08 10:22:51 -0400

    Added comment form

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
