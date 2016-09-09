57c3958165762e4b1be5e5d05c4284dd1a2bfe82
Francis Rocco <roccofrancis@gmail.com>
2016-09-09 15:09:39 -0400

    add tag show page to show all posts associated

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,8 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :tags
   has_many :comments
 
-  def thumbnail
-    object = LinkThumbnailer.generate(self.link)
-  end
-
   def post_title
-    return self.title unless title = self.thumbnail.title
+    return self.title unless title = @object.title
     if title.length < 35
       return title
     else
@@ -17,8 +13,8 @@ class Post < ApplicationRecord
   end
 
   def description
-    desc = self.thumbnail.description
-    desc = self.title if self.thumbnail.images.first.src.to_s[-3..-1] != "jpg"
+    desc = @object.description
+    desc = self.title if @object.images.first.src.to_s[-3..-1] != "jpg"
     if desc.length < 120
       return desc
     else
@@ -27,11 +23,12 @@ class Post < ApplicationRecord
   end
 
   def favicon
-    self.thumbnail.favicon
+    @object.favicon
   end
 
   def image
-    img = self.thumbnail.images.first.src.to_s
+    @object = LinkThumbnailer.generate(self.link)
+    img = @object.images.first.src.to_s
     if img[-3..-1] != "jpg"
       return "http://img00.deviantart.net/4854/i/2013/352/8/1/newspaper_collage_texture_by_flordeneu-d6yeuvs.jpg"
     else
