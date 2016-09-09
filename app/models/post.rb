b3f6f3ffd33ae9077e1f35010499fd488ae00558
Clifton Regis <github email address>
2016-09-09 15:39:34 -0400

    fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,41 +2,58 @@ class Post < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :tags
   has_many :comments
+  has_many :votes, dependent: :destroy
+  
+  # def thumbnail
+  #   @object = LinkThumbnailer.generate(self.link)
+  # end
 
-  def thumbnail
-    object = LinkThumbnailer.generate(self.link)
+  # def post_title
+  #   return self.title unless title = @object.title
+  #   if title.length < 35
+  #     return title
+  #   else
+  #     return title[0..33] + "..."
+  #   end
+  # end
+
+  # def description
+  #   desc = self.thumbnail.description
+  #   desc = self.title if self.thumbnail.images.first.src.to_s[-3..-1] != "jpg"
+  #   if desc.length < 120
+  #     return desc
+  #   else
+  #     return desc[0..117] + "..."
+  #   end
+  # end
+
+  # def favicon
+  #   self.thumbnail.favicon
+  # end
+
+  # def image
+  #   img = self.thumbnail.images.first.src.to_s
+  #   if img[-3..-1] != "jpg"
+  #     return "http://img00.deviantart.net/4854/i/2013/352/8/1/newspaper_collage_texture_by_flordeneu-d6yeuvs.jpg"
+  #   else
+  #     return img
+  #   end
+  # end
+
+  #Methods for Votes function
+
+  def up_votes
+    self.votes.where(value: 1).count
   end
 
-  def post_title
-    return self.title unless title = self.thumbnail.title
-    if title.length < 35
-      return title
-    else
-      return title[0..33] + "..."
-    end
+  def down_votes
+    self.votes.where(value: -1).count
   end
+ 
+  def total_votes
+    self.up_votes - self.down_votes
+  end  
 
-  def description
-    desc = self.thumbnail.description
-    desc = self.title if self.thumbnail.images.first.src.to_s[-3..-1] != "jpg"
-    if desc.length < 120
-      return desc
-    else
-      return desc[0..117] + "..."
-    end
-  end
-
-  def favicon
-    self.thumbnail.favicon
-  end
-
-  def image
-    img = self.thumbnail.images.first.src.to_s
-    if img[-3..-1] != "jpg"
-      return "http://img00.deviantart.net/4854/i/2013/352/8/1/newspaper_collage_texture_by_flordeneu-d6yeuvs.jpg"
-    else
-      return img
-    end
-  end
+  
 
 end
