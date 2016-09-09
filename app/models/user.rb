c002bde0099f5a6d48bdf6c834d40079ebca5fe3
dowdje <dowdje89@gmail.com>
2016-09-09 18:56:31 -0400

    Committing to pull

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-  has_many :followships, foreign_key: 'follower_id'
-  # has_many :followers, through: :followships, foreign_key: 'follower_id'
-  # has_many :followings, through: :followships, foreign_key: 'following_id'
+  # has_many :followships
+  def followers
+    Followship.where(following_id: self.id)
+  end
+
+  def following
+    Followship.where(follower_id: self.id)
+  end
+  # has_many :followships
+  has_many :followers, class_name: 'Followships'
+  has_many :following, class_name: 'Followships'
 	has_many :votes, dependent: :destroy
 	has_many :posts
     has_many :comments
@@ -19,40 +27,40 @@ class User < ApplicationRecord
 
   # think of 'following' as 'followee' and it makes sense to Tamtam
 
-	def followers
-		self.class.joins(:followships).where(followships: {following_id: self.id})
-	end
+	# def followers
+	# 	self.class.joins(:followships).where(followships: {following_id: self.id})
+	# end
 
-	# return everyone that a user is following
-	def following
-		self.class.joins(:followships).where(followships: {follower_id: self.id})
-	end
+	# # return everyone that a user is following
+	# def following
+	# 	self.class.joins(:followships).where(followships: {follower_id: self.id})
+	# end
 
 	# self.following?(user) => is self following user?
-	def following?(user)
-		return true ? following : false
-	end
+	# def following?(user)
+	# 	return true ? following : false
+	# end
 
-	# self.followed_by?(user) => is self followed by user?
-	def followed_by?(user)
-		return true ? followers : false
-	end
+	# # self.followed_by?(user) => is self followed by user?
+	# def followed_by?(user)
+	# 	return true ? followers : false
+	# end
 
-	def has_followers?
-	 self.followers.size > 0
-	end
+	# def has_followers?
+	#  self.followers.size > 0
+	# end
 
-	def has_following?
-	 self.following.size > 0
-	end
+	# def has_following?
+	#  self.following.size > 0
+	# end
 
-	def friends?(user)
-		if self.following?(user) && self.followed_by?(user)
-			return true 
-		else
-			return false
-		end
-	end
+	# def friends?(user)
+	# 	if self.following?(user) && self.followed_by?(user)
+	# 		return true 
+	# 	else
+	# 		return false
+	# 	end
+	# end
 
 
 end
