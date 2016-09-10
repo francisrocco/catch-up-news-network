521704b33c8b8737d1d14260ed495ddabb57ebab
dowdje <dowdje89@gmail.com>
2016-09-10 10:25:34 -0400

    Uncommented some thumbnailer methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
 		render 'sessions/new'
 	end
 
-	def current_user
-	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+	def current_user 
+	  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
     
 
