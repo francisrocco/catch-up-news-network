e1d895ca92d21ec423708b08ca2d5faab039c02a
Tami <catquarks@gmail.com>
2016-09-09 20:59:48 -0400

    made follower_count and following_count mthods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 	has_many :followerships, foreign_key: 'follower_id', class_name: 'Followship'
 	has_many :followingships, foreign_key: 'following_id', class_name: 'Followship'
 
-	# current user follows a new user: current_user.follow(user) => current user is now following user
+	# self follows a new user
   def follow(user)
     new_relationship = Followship.new(following_id: user.id, follower_id: self.id)
     if new_relationship.save
@@ -17,7 +17,7 @@ class User < ApplicationRecord
     end
   end
 
-	# return everyone following the user
+	# return everyone following self
   def followers
   	# this returns an array, which is a bit inconvenient
   	# self.class.find_by_sql(['SELECT * FROM users JOIN followships ON users.id = followships.following_id WHERE followships.following_id = ?', self.id])
@@ -26,10 +26,17 @@ class User < ApplicationRecord
   	self.class.joins(:followerships).where(followships: {following_id: self.id})
   end
 
-	# return everyone that a user is following
+	# return everyone that self is following
   def following
-    # self.class.find_by_sql(["SELECT * FROM users JOIN followships ON users.id = followships.following_id WHERE followships.follower_id = ?", self.id])
     self.class.joins(:followingships).where(followships: {follower_id: self.id})
+  end
+
+  def followers_count
+  	followers.count
+  end
+
+  def following_count
+  	following.count 
   end
 
 	# is anyone following self?
@@ -47,6 +54,9 @@ class User < ApplicationRecord
 		if self.has_following?
 			if self.following.where(name: user.name).exists?
 				return true
+			else
+				# we need this because it otherwise returns 'nil' and 'nil' isn't falsey enough!
+				return false
 			end
 		else
 			return false
@@ -58,6 +68,9 @@ class User < ApplicationRecord
 		if self.has_following?
 			if self.followers.where(name: user.name).exists?
 				return true
+			else
+				# we need this because it otherwise returns 'nil' and 'nil' isn't falsey enough!
+				return false
 			end
 		else
 			return false
