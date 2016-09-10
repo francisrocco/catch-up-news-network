d822b35a5ef549c48844a54e0eb9ba40b9d23129
Tami <catquarks@gmail.com>
2016-09-10 19:11:50 -0400

    making parse_comment method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,19 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def is_new_comment?
+    seconds = Time.now - self.created_at
+    seconds < 300 ? true : false
+  end
+
+  def get_datetime
+    if is_new_comment?
+      "Just now:"
+    else
+      self.created_at.strftime('%l:%M %P, on %a, %D:')
+    end
+  end
+
+
 end
