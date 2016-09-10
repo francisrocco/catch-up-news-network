4b705c5201b58f86bc50a44e94c981827b3b8818
Tami <catquarks@gmail.com>
2016-09-10 19:37:11 -0400

    comments are in reverse chron order

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,12 +7,21 @@ class Comment < ApplicationRecord
     seconds < 300 ? true : false
   end
 
-  def get_datetime
+  def datetime
     if is_new_comment?
-      "Just now:"
+      "just now"
     else
-      self.created_at.strftime('%l:%M %P, on %a, %D:')
+      self.created_at.strftime('on %a, %D at %l:%M %P')
     end
+  end
+
+  def author
+  	self.post.user.name
+  end
+
+  # not using this, but maybe it'll become useful?
+  def parse_comment
+  	"#{author} on #{datetime}: #{self.message}"
   end
 
 
