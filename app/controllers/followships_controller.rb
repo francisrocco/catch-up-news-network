2537f3ea2cda2f3d8a0eb37385322ed6f40a13ae
Tami <catquarks@gmail.com>
2016-09-11 02:05:35 -0400

    fix bug when following users but still need to reformat flash notices when you successfully follow a user, because right now it appears to be an error

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -8,7 +8,7 @@ class FollowshipsController < ApplicationController
     if user.followed_by?(current_user)
       flash[:notice] = "You're already following this user!"
       redirect_to user_path(user)
-    elsif user = current_user
+    elsif user == current_user
       flash[:notice] = "You can't follow yourself, silly!"
       redirect_to user_path(user)
     else
