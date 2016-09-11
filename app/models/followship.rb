9424dbab02af9333f0d425c2a5ec136bf8ce6210
Tami <catquarks@gmail.com>
2016-09-10 23:54:17 -0400

    add validation so users cant follow themselves

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -4,4 +4,12 @@ class Followship < ApplicationRecord
 
   validates_uniqueness_of :follower_id, :scope => :following_id
 
+  validate :follower_cant_be_following
+
+  def follower_cant_be_following
+  	unless follower_id != following_id
+  		errors.add(:followship, "Oh dear me, you can't follow yourself, silly!")
+  	end
+  end
+
 end
