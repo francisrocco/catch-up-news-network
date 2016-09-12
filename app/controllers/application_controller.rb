6e7f657e731b688e36a33d944803a97f707d1635
Francis Rocco <roccofrancis@gmail.com>
2016-09-12 10:52:35 -0400

    bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,10 @@ class ApplicationController < ActionController::Base
 		end
 	end
 
-	def current_user 
-	  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
-    
+
 
 	def logged_in?
 	  !!current_user
