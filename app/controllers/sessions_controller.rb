d4c0dbf5fd1ffee79b1d44b6168446304f5ba27c
Tami <catquarks@gmail.com>
2016-09-12 15:25:16 -0400

    fix error message when logging in incorrectly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to posts_path
     else
-      flash[:notice] = "Wrong email / password!"
+      flash[:error] = "Wrong email / password!"
       render :new
     end
   end
