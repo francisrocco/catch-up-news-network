e0747050e784f144283241502a15db2dadb5d9d0
Francis Rocco <roccofrancis@gmail.com>
2016-09-12 16:59:38 -0400

    finish ajax-ifying follows

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -4,10 +4,10 @@ class FollowshipsController < ApplicationController
 
   def follow
     user = User.find(params[:id])
-
     current_user.follow(user)
+    
     respond_to do |f|
-      f.js
+      f.js { render json: {user_id: user.id} }
       f.html { redirect_to root_path }
     end
   end
