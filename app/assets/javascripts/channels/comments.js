0563423f6890e31a5eab7f3d68f3bf5fddaba530
Tami <catquarks@gmail.com>
2016-09-13 10:22:31 -0400

    fix form clearing thing

--- app/assets/javascripts/channels/comments.js
+++ app/assets/javascripts/channels/comments.js
@@ -1,12 +1,14 @@
 App.comments = App.cable.subscriptions.create('CommentsChannel', {  
   received: function(data) {
+
     return $('#comments').prepend(this.renderComment(data));
 
   },
 
   renderComment: function(data) {
-  	"<p> <span class='comment'> <strong>" + "<a href='users/" + data.user_id + "'>" + data.user + "</a> " + data.datetime + ": </strong>" + data.comment + "</span></p>";
   	$('textarea').val('');
+  	return "<p> <span class='comment'> <strong>" + "<a href='users/" + data.user_id + "'>" + data.user + "</a> " + data.datetime + ": </strong>" + data.comment + "</span></p>"
+  	
 
   }
 });
