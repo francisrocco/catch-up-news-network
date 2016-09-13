92d9c6430de768034351015b743620346d820a39
dowdje <dowdje89@gmail.com>
2016-09-13 11:08:34 -0400

    Added Air Horn Sound to Comment Web Socket

--- app/assets/javascripts/channels/comments.js
+++ app/assets/javascripts/channels/comments.js
@@ -6,6 +6,8 @@ App.comments = App.cable.subscriptions.create('CommentsChannel', {
   },
 
   renderComment: function(data) {
+    var horn = $('#audio');
+    horn.get(0).play();
   	$('textarea').val('');
   	return "<p> <span class='comment'> <strong>" + "<a href='users/" + data.user_id + "'>" + data.user + "</a> " + data.datetime + ": </strong>" + data.comment + "</span></p>"
   	
