cbda02b6bf4c6a796e80a1d24eeaad2a52180594
dowdje <dowdje89@gmail.com>
2016-09-14 16:51:21 -0400

    Fixed Sound File

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,5 +2,12 @@ require 'rails_helper'
 
 describe Comment, type: :model do
   it 'has an author' do
+    comment = Comment.create(message: "This is a terrible post!")
+    comment.user = User.new(name: "Kung Fu Master")
+    expect(comment.user.name).to eq("Kung Fu Master")
+  end
+
+  it 'belongs to a post' do
+    expect(true).to eq("Kung Fu Master")
   end
 end
