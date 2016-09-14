b3c6e8f9f5149b314f4b0a095753d533ac8d53ec
Tami <catquarks@gmail.com>
2016-09-14 18:15:53 -0400

    add yield block to user helper!

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+
+	def display_user_relationship(user, current_user)
+		if current_user.friends_with?(user)
+			return "You're both friends!"
+		end
+
+		if current_user.following?(user)
+			return "You're following #{user}!"
+		elsif current_user.followed_by?(user)
+			return "#{user} is following you!"
+		end
+	end
+
+	def display_follow_link(user, current_user)
+		if current_user != user && !current_user.following?(user)
+			yield
+		end
+	end
+
 end
