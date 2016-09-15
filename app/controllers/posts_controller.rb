ae5380c44ca84cc0efc88f1c28aee145078b288b
Clifton Regis <github email address>
2016-09-15 09:59:33 -0400

    Create Thumbnail Decorator and Post Paginate service

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,7 +64,7 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @post = ThumbnailDecorator.new(Post.find(params[:id]))
+    @post = Post.find(params[:id])
   end
 
   def post_params
